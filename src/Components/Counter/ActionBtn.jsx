e29dbd20a0a2d88373aec6f1f52ebc56a5a016c0
MohamedAl-Attar <mohamedelattar28@yahoo.com>
2023-03-04 06:57:53 +0200

    \

--- src/Components/Counter/ActionBtn.jsx
+++
@@ -1,9 +0,0 @@
-
-import Button from 'react-bootstrap/Button';
-function ActionBtn(props){
-    return(
-        <Button style={{width : '15%'}} variant={props.variant} onClick = {props.method}>{props.name}</Button>
-    )
-}
-
-export default ActionBtn;
--- src/Components/Counter/Counter.jsx
+++
@@ -1,9 +0,0 @@
-
-
-function Counter(props){
-    return(
-    <h1>{props.cnt}</h1>
-    )
-}
-
-export default Counter;
